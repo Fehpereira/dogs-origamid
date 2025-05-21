9a439be0649c7699954e3a985a495a0f73aa569c
Felipe Pereira <fehpereira237@gmail.com>
2025-05-21 15:47:36 -0300

    criando e estilizando o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import Dogs from '../assets/dogs.svg?react';
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/">
+          <Dogs aria-label="Dogs - Home" />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
     </div>
   );
