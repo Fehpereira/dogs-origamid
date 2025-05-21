16fd82eda268d3ba2d57da7bd4d3b8c32edb74f6
Felipe Pereira <fehpereira237@gmail.com>
2025-05-21 12:07:07 -0300

    estruturação inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+//components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
